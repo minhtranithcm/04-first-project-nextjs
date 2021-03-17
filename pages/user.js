9b4f669d053919a1d6a953e618982f45eebbb634
mintran <minhtranit.hcm@gmail.com>
2021-03-17 14:41:41 +0700

    first commit 2

---
+++ pages/user.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+user.propTypes = {
+    
+};
+
+function user(props) {
+    return (
+        <div>
+            <h1>Hello user</h1>    
+        </div>
+    );
+}
+
+export default user;
